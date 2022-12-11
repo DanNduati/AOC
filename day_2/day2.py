6bfce94a66cca1cf4d53d521d75ef20e1d1e061e
DanNduati <dannduati2@gmail.com>
2022-12-12 01:48:43 +0300

    Day 2 puzzle 2

--- day_2/day2.py
+++ day_2/day2.py
@@ -15,10 +15,11 @@ Scores
 """
 opponent_dict = {"A": "Rock", "B": "Paper", "C": "Scissors"}
 me_dict = {"X": "Rock", "Y": "Paper", "Z": "Scissors"}
-total_score = 0
+total_score = total_score_2 = 0
 with open("input.txt", "r") as fp:
-    for opp,me in map(str.split, fp.read().splitlines()):
+    for opp, me in map(str.split, fp.read().splitlines()):
         opp, me = ord(opp) - ord("A"), ord(me) - ord("X")
         total_score += 1 + me + (me - opp + 1) % 3 * 3
-
-print (total_score)
+        total_score_2 += 1 + me + (me + opp - 1) % 3
+print(total_score)
+print(total_score_2)
