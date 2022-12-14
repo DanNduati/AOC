08fa4f9bc628515961da508248ecc495befba262
DanNduati <dannduati2@gmail.com>
2022-12-14 12:35:17 +0300

    Solve day4 part 2

--- day_4/day4.py
+++ day_4/day4.py
@@ -9,18 +9,31 @@ Camp Cleanup
 """
 
 
-def check_contained(a, b: list[str]) -> bool:
-    range_a = list(range(int(a.split("-")[0]), int(a.split("-")[1]) + 1))
-    range_b = list(range(int(b.split("-")[0]), int(b.split("-")[1]) + 1))
-    if set(range_a).issubset(set(range_b)) or set(range_b).issubset(set(range_a)):
+def check_subset(a, b: list[str]) -> bool:
+    range_a = set(range(int(a.split("-")[0]), int(a.split("-")[1]) + 1))
+    range_b = set(range(int(b.split("-")[0]), int(b.split("-")[1]) + 1))
+    if range_a.issubset(range_b) or range_b.issubset(range_a):
         return True
     return False
 
-count = 0
+
+def check_overlap(a, b: list[str]) -> bool:
+    range_a = set(range(int(a.split("-")[0]), int(a.split("-")[1]) + 1))
+    range_b = set(range(int(b.split("-")[0]), int(b.split("-")[1]) + 1))
+    if range_a.intersection(range_b):
+        return True
+    return False
+
+
+subset_count = overlap_count = 0
 with open("input.txt", "r") as fp:
     for line in fp:
         pair = line.strip()
         a, b = pair.split(",")
-        if (check_contained(a, b)):
-            count+=1
-print(count)
+        if check_subset(a, b):
+            subset_count += 1
+        if check_overlap(a, b):
+            overlap_count += 1
+print(
+    f"Assignment pairs that full contain the other {subset_count}\nAssignment pairs with overlaping ranges {overlap_count}"
+)
